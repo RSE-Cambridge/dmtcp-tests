21eedff6dcb31587bf992af29a555ebbf8625c93
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-01-03 17:06:50 +0000

    The example executable now doesn't print the name of the executable as a command line argument

--- example.cpp
+++ example.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     using namespace std::chrono; 
     
     std::cout << "Given command line arguments: " << std::endl;
-    for (int i = 0; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
         std::cout << argv[i] << std::endl;
     }
