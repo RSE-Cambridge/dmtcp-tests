619fbdd43352e27bdbd9c42f29e31d98e08cdf40
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-01-03 16:04:02 +0000

    Removed from the scripts the ability to choose dmtcp version

--- run_long.sh
+++ run_long.sh
@@ -1,49 +1,25 @@
 #!/bin/bash
 RESTARTSCRIPT="dmtcp_restart_script.sh"
-#RESTARTSCRIPT="ckpt*"
 
 if [ -f "$RESTARTSCRIPT" ]
 then
     echo "Start with checkpoint executable"
-    #eval "../kill_long.sh &"
 
-    if [[ "$1" -eq "1" ]];
-    then
-      eval "dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
-      sleep 1
-      cport=$(<cport.txt)
-      echo "$cport"
-      CMD="dmtcp_restart -q -i 5 -p "$cport" ./ckpt_*.dmtcp" 
-      echo $CMD
-      eval $CMD
-    else
-      eval "dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
-      sleep 1
-      cport=$(<cport.txt)
-      echo "$cport"
-      CMD="dmtcp_restart -q -i 5 -p "$cport" ./ckpt_*.dmtcp" 
-      echo $CMD
-      eval $CMD
-    fi
+    eval "dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
+    sleep 1
+    cport=$(<cport.txt)
+    echo "$cport"
+    CMD="dmtcp_restart -q -i 5 -p "$cport" ./ckpt_*.dmtcp" 
+    echo $CMD
+    eval $CMD
 else
     echo "Start new executable"
-    #eval "../kill_long.sh &"
-    if [[ "$1" -eq "1" ]];
-    then
-      eval "dmtcp_coordinator -q  --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
-      sleep 1
-      cport=$(<cport.txt)
-      echo "$cport"
-      CMD="dmtcp_launch -q -i 5 -p "$cport" ../example"
-      echo $CMD
-      eval $CMD
-    else
-      eval "dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
-      sleep 1
-      cport=$(<cport.txt)
-      echo "$cport"
-      CMD="dmtcp_launch -q -i 5 -p "$cport" ../example"
-      echo $CMD
-      eval $CMD
-    fi
+    
+    eval "dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
+    sleep 1
+    cport=$(<cport.txt)
+    echo "$cport"
+    CMD="dmtcp_launch -q -i 5 -p "$cport" ../example"
+    echo $CMD
+    eval $CMD
 fi
--- run_test.sh
+++ run_test.sh
@@ -6,6 +6,6 @@ rm dmtcp*
 counter=1
 while [ $counter -le 5 ]
 do
-  ../run_long.sh $1
+  ../run_long.sh
   ((counter++))
 done
