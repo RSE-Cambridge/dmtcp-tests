0389541cb950f177cb3d17cacadd9067ac13d3de
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-01-03 15:49:41 +0000

    Changed scripts to use the dmtcp/2.3.1 module

--- run_long.sh
+++ run_long.sh
@@ -17,11 +17,11 @@ then
       echo $CMD
       eval $CMD
     else
-      eval "/home/ea461/dmtcp/dmtcp-2.3.1/bin/dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
+      eval "dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
       sleep 1
       cport=$(<cport.txt)
       echo "$cport"
-      CMD="/home/ea461/dmtcp/dmtcp-2.3.1/bin/dmtcp_restart -q -i 5 -p "$cport" ./ckpt_*.dmtcp" 
+      CMD="dmtcp_restart -q -i 5 -p "$cport" ./ckpt_*.dmtcp" 
       echo $CMD
       eval $CMD
     fi
@@ -38,11 +38,11 @@ else
       echo $CMD
       eval $CMD
     else
-      eval "/home/ea461/dmtcp/dmtcp-2.3.1/bin/dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
+      eval "dmtcp_coordinator -q --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
       sleep 1
       cport=$(<cport.txt)
       echo "$cport"
-      CMD="/home/ea461/dmtcp/dmtcp-2.3.1/bin/dmtcp_launch -q -i 5 -p "$cport" ../example"
+      CMD="dmtcp_launch -q -i 5 -p "$cport" ../example"
       echo $CMD
       eval $CMD
     fi
