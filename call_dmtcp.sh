cc29eddf1f765bcc1396c6091ee48a2752f6266b
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-02-21 21:17:35 +0000

    Update call_dmtcp.sh

--- call_dmtcp.sh
+++ call_dmtcp.sh
@@ -13,19 +13,19 @@ then
     sleep 1
     cport=$(<cport.txt)
     echo "$cport"
-    CMD="dmtcp_restart -i "$tint" -p "$cport" ./ckpt_*.dmtcp" 
+    CMD="dmtcp_restart --ckptdir ckpoints -i "$tint" -p "$cport" ckpoints/ckpt_*.dmtcp"
     echo $CMD
     eval $CMD
 else
     echo "Start new executable"
-    
+
     eval "dmtcp_coordinator --exit-after-ckpt --exit-on-last --port-file cport.txt -p 0 &"
     sleep 1
     cport=$(<cport.txt)
     echo "$cport"
-    CMD="dmtcp_launch --ckpt-open-files -i "$tint" -p "$cport" "$runcmd
+    CMD="dmtcp_launch --no-gzip --ckptdir ckpoints --ckpt-open-files -i "$tint" -p "$cport" "$runcmd
     echo $CMD
     eval $CMD
 fi
 
-sleep 60s
+sleep 60
